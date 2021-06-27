d0e83787696c3b6487c7db9c58dc3390c4e9bf6c
Noriki Nakamura <bynn_chapu@tencho-lab.net>
2021-06-27 22:48:24 +0900

    Add ContainerImage materials.

---
+++ ContainerImage/register-route53.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import json
+import os
+import boto3
+
+
+def get_task_arn(client):
+    response = client.list_tasks(
+        cluster=os.getenv('CLUSTER'),
+        serviceName=os.getenv('SERVICE'),
+        desiredStatus='RUNNING'
+    )
+    taskArn = response['taskArns'][0]
+    
+    return taskArn
+
+
+def get_task_eni(client, taskArn):
+    response = client.describe_tasks(
+        cluster=os.getenv('CLUSTER'),
+        tasks=[taskArn]
+    )
+    eniId = response['tasks'][0]['attachments'][0]['details'][1]['value']
+    
+    return eniId
+
+
+def get_publicip_eni(eniId):
+    eniInfo = boto3.resource('ec2').NetworkInterface(eniId)
+    
+    return eniInfo.association_attribute['PublicIp']
+
+
+def main():
+    client = boto3.client('ecs')
+    taskArn = get_task_arn(client)
+    print('taskArn: ' + taskArn)
+    
+    eniId = get_task_eni(client, taskArn)
+    print('eniId: ' + eniId)
+    
+    publicIp = get_publicip_eni(eniId)
+    print('Public IP: ' + publicIp)
+
+
+if __name__ == "__main__":
+    main()
